30f674900e971e17f56494ba90654ebcb2674eee
astratow <davno_pora@mail.com>
2021-05-07 04:56:23 +0100

    multiplyx2x3  and changes in readme and todo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JS_playground
+This is a repostiory to help understand JavaScript and colaborate on some little programs.
---
+++ multiplyx2x3/app.js
@@ -0,0 +1,9 @@
+/*app.js*/
+function mult(value) {
+	var x,y;
+	x = 2 * value;
+	y = 3 * value;
+
+	document.getElementById('out2x').value = x;
+	document.getElementById('out3x').value = y;
+}
